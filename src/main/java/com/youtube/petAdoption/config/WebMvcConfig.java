240f29b8b23c104247f942687393f950a5352a91
Htoo Thet Naung <htoothetnaung295@gmail.com>
2025-03-09 23:20:20 +0630

    Modify websecurity

--- src/main/java/com/youtube/petAdoption/config/WebMvcConfig.java
+++ src/main/java/com/youtube/petAdoption/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins(
-                "https://pawxcatalog.netlify.app/",
+                "https://pawxcatalog.netlify.app",
                 "http://localhost:4200"
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
